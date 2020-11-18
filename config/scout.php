9a6cee3db94779057a6850518676dd51b49c1e86
rodzy <irod2899@gmail.com>
2020-11-18 23:01:47 +0000

    Updated scouting configuration

--- config/scout.php
+++ config/scout.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'connection' => env('DB_CONNECTION', 'pgsql'),
+    'maintain_index' => true,
+    'config' => 'english',
+    'search_using' => 'tsquery',
     'driver' => env('SCOUT_DRIVER', 'pgsql'),
     'prefix' => env('SCOUT_PREFIX', ''),
     'queue' => false,
